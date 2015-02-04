1d90ab49091569db9746489e7a02d04ad22a161a
quantumhair <jamoody@gmail.com>
2015-02-04 13:46:14 -0500

    updated database

--- webupdownapp/rssrecords/migrations/0001_initial.py
+++ webupdownapp/rssrecords/migrations/0001_initial.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
+import datetime
 import shortuuidfield.fields
 from django.conf import settings
 
@@ -19,9 +20,13 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('uuid', shortuuidfield.fields.ShortUUIDField(unique=True, max_length=22, editable=False, blank=True)),
                 ('name', models.CharField(max_length=200)),
-                ('url', models.URLField(max_length=150)),
+                ('url', models.URLField(max_length=250)),
                 ('desc', models.TextField(blank=True)),
                 ('created_on', models.DateField(auto_now_add=True)),
+                ('last_checked', models.DateField(default=datetime.date(1970, 1, 1), auto_now=True)),
+                ('upordown', models.CharField(default=b'not yet checked', max_length=15)),
+                ('dayssinceupdate', models.IntegerField(default=9999)),
+                ('group', models.CharField(default=b'No Group', max_length=100)),
                 ('owner', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
             ],
             options={
